acb7781cade0b1b39a59ba1b123dc37f73535241
gregorydaval345 <davalgregory69@gmail.com>
2022-07-17 22:57:09 +0100

    WIP: Installed ethers and created a page where I will be doing the logic and fetching

---
+++ pages/FetchDataEth.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ethers } from "ethers";
+
+function FetchDataEth() {
+  return (
+    <div>
+        Lets fetch some blockchain data
+    </div>
+  )
+}
+
+export default FetchDataEth
