c6c471e0ddb6cbb0877b3050e12316a3603c6e09
gregorydaval345 <davalgregory69@gmail.com>
2022-07-19 21:49:22 +0100

    Added the two functions

--- pages/FetchDataEth.js
+++ pages/FetchDataEth.js
@@ -5,6 +5,15 @@ const provider = new ethers.providers.JsonRpcProvider(
   `https://mainnet.infura.io/v3/7731b280b7ed4b8ebbd0a5df73efc86b`
 );
 
+
+function fetchMint() {
+
+}
+
+function fetchBurn() {
+  
+}
+
 const FetchDataEth = async () => {
   const contractAddress = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
 
