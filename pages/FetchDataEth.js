fb159e8f34d603be54cbfe066ed57f909ea2de94
gregorydaval345 <davalgregory69@gmail.com>
2022-07-19 07:57:40 +0100

    Two functions, fetchMint and fetchBurn to get the last 20 tnx from wBTC

--- pages/FetchDataEth.js
+++ pages/FetchDataEth.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from "react";
 import { ethers } from "ethers";
 
-function FetchDataEth() {
-  return (
-    <div>
-        Lets fetch some blockchain data
-    </div>
-  )
-}
+const provider = new ethers.providers.JsonRpcProvider(
+  `https://mainnet.infura.io/v3/7731b280b7ed4b8ebbd0a5df73efc86b`
+);
 
-export default FetchDataEth
+const FetchDataEth = async () => {
+  const contractAddress = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
+
+  const balance = await provider.getBalance(contractAddress);
+
+  console.log(
+    `\nETH Balance of ${contractAddress} --> ${ethers.utils.formatEther(
+      balance
+    )} ETH\n`
+  );
+
+  return <div>${balance}</div>;
+};
+
+export default FetchDataEth;
