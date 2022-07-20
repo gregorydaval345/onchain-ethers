cc4bdf570f5287162ccf4aee9a639e563cec585d
gregorydaval345 <davalgregory69@gmail.com>
2022-07-20 11:16:54 +0100

    WIP: Added Token contract and filters, including queryFilter

--- pages/FetchDataEth.js
+++ pages/FetchDataEth.js
@@ -1,31 +1,29 @@
 import React from "react";
 import { ethers } from "ethers";
+import abi from "../utils/abi.json";
 
 const provider = new ethers.providers.JsonRpcProvider(
   `https://mainnet.infura.io/v3/7731b280b7ed4b8ebbd0a5df73efc86b`
 );
 
+function fetchMint() {}
 
-function fetchMint() {
-
-}
-
-function fetchBurn() {
-  
-}
+function fetchBurn() {}
 
 const FetchDataEth = async () => {
   const contractAddress = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
 
   const balance = await provider.getBalance(contractAddress);
 
-  console.log(
-    `\nETH Balance of ${contractAddress} --> ${ethers.utils.formatEther(
-      balance
-    )} ETH\n`
-  );
+  const tokenContract = new ethers.Contract(contractAddress, abi, provider);
 
-  return <div>${balance}</div>;
+  let filter = tokenContract.filters.Mint();
+
+  let eventsQueryData = await tokenContract.queryFilter(filter, -150000);
+
+  console.log(eventsQueryData);
+
+  return <div>{eventsQueryData}</div>;
 };
 
 export default FetchDataEth;
